25d708403d58eece551edd908d8470dfd5e4479f
Diego <diego2000avelar@gmail.com>
2021-05-03 19:25:06 -0600

    configure graphql

--- src/core/prisma/seed.ts
+++ src/core/prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '.prisma/client';
 import * as fs from 'fs';
 import { join } from 'path';
 import shuffleArray from '../../common/helpers/shuffle-array.helper';
