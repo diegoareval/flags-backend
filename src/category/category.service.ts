698bdabedce17018d1426aa686f462fd83f835a1
Diego <diego2000avelar@gmail.com>
2021-05-04 19:50:25 -0600

    advance

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -2,7 +2,14 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class CategoryService {
-    async getMany(){}
+    async getMany(){
+        return [{
+            id: '37373',
+            createdAt: new Date(),
+            updatedAt: new Date(),
+            name: 'Category'
+        }]
+    }
     async get(){}
     async create(){}
     async update(){}
