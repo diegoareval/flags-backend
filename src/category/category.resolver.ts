f6e04a2b439939f59b2b13839d0c30dd18f63893
Diego <diego2000avelar@gmail.com>
2021-05-04 20:42:20 -0600

    dto created

--- src/category/category.resolver.ts
+++ src/category/category.resolver.ts
@@ -1,12 +1,27 @@
+import { CreateCategoryDto } from './dto/create-category-dto';
 import { CategoryService } from './category.service';
 import { Category } from './model/category';
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
 
 @Resolver(() => Category)
 export class CategoryResolver {
   constructor(private readonly categoryService: CategoryService) {}
+
   @Query(() => [Category])
   async categories() {
     return await this.categoryService.getMany();
+  }
+
+  @Query(() => Category)
+  async category(@Args('id') id: string) {
+    return await this.categoryService.get(id);
+  }
+
+  @Mutation(() => Category)
+  async createCategory(
+    @Args({ name: 'input', type: () => CreateCategoryDto })
+    data: CreateCategoryDto,
+  ) {
+    return await this.categoryService.create(data);
   }
 }
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,3 +1,4 @@
+import { CreateCategoryDto } from './dto/create-category-dto';
 import { PrismaService } from './../core/prisma/prisma.service';
 import { Injectable } from '@nestjs/common';
 
@@ -5,10 +6,25 @@ import { Injectable } from '@nestjs/common';
 export class CategoryService {
   constructor(private readonly prismaService: PrismaService) {}
   async getMany() {
-    return await this.prismaService.category.findMany();
+    return await this.prismaService.category.findMany({
+      include: { questions: { include: { answers: true } } },
+    });
   }
-  async get() {}
-  async create() {}
+  async get(id: string) {
+    return await this.prismaService.category.findUnique({
+      where: { id },
+      include: { questions: { include: { answers: true } } },
+    });
+  }
+  async create(data: CreateCategoryDto) {
+    return await this.prismaService.category.create({
+      data,
+      include: { questions: { include: { answers: true } } },
+    });
+  }
   async update() {}
-  async delete() {}
+
+  async delete(id: string) {
+    return await this.prismaService.category.delete({ where: { id } });
+  }
 }
