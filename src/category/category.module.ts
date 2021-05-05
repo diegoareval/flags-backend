d30066fc742674a42b16ad958e0518f40d7385b5
Diego <diego2000avelar@gmail.com>
2021-05-04 19:55:21 -0600

    creating prisma module

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,8 +1,10 @@
+import { PrismaModule } from './../core/prisma/prisma.module';
 import { Module } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CategoryResolver } from './category.resolver';
 
 @Module({
+  imports: [PrismaModule],
   providers: [CategoryService, CategoryResolver]
 })
 export class CategoryModule {}
